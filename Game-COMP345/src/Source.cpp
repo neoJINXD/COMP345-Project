7670d9caf449deb0d239debc27f8117f9950e361
neo <anik_patel@live.com>
2020-02-10 00:21:32 -0500

    Setup base for Player class

--- Game-COMP345/src/Source.cpp
+++ Game-COMP345/src/Source.cpp
@@ -1,8 +1,10 @@
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	/*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Cyan);
 
@@ -18,7 +20,10 @@ int main()
 		window.clear();
 		window.draw(shape);
 		window.display();
-	}
+	}*/
+
+	std::cout << "ZA WARUDO" << std::endl;
+	std::cin.get();
 
 	return 0;
 }
