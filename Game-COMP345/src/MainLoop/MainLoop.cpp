49047fb482037c3bb8acb8d3f5c856a2ceb8c135
Anik Patel <neojinxd@gmail.com>
2020-04-02 00:48:13 -0400

    MainLoop driver test

--- Game-COMP345/src/MainLoop/MainLoop.cpp
+++ Game-COMP345/src/MainLoop/MainLoop.cpp
@@ -1,5 +1,26 @@
 #include "MainLoop.h"
 
-void maingame::MainLoop::turn()
+void maingame::MainLoop::init(int numberOfPlayer)
 {
+	// finding number of empty tiles the board will start with
+	switch (numberOfPlayer)
+	{
+	case 2:
+		freeTiles = 5 * 5;
+		break;
+	case 3:
+		freeTiles = 5 * 7;
+		break;
+	case 4:
+		freeTiles = 7 * 7 - 4;
+		break;
+	default:
+		std::cout << "no" << "bad boomer" << std::endl;
+		exit(-404);
+	}
+}
+
+maingame::MainLoop::MainLoop(int numberOfPlayer)
+{
+	init(numberOfPlayer);
 }
--- Game-COMP345/src/MainLoop/MainLoop.h
+++ Game-COMP345/src/MainLoop/MainLoop.h
@@ -4,15 +4,35 @@
 #define new new( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 #endif
 
+#include <list>
+#include <iostream>
+#include "../Player/Player.h"
+
 namespace maingame
 {
 	class MainLoop
 	{
 	private: 
-		MainLoop() {}
+		//MainLoop() {}
+
+		std::list<player::Player*>* queue;
+		player::Player* activePlayer;
+
+		int freeTiles;
+		void init(int numberOfPlayer);
 
 	public:
-		void turn();
+		MainLoop(int numberOfPlayer);
+		~MainLoop();
+		
 
+	};
+
+
+
+	class MainLoopDriver
+	{
+	public:
+		void run();
 	};
 }
